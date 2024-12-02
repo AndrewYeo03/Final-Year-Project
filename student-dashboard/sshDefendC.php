21d69e83e526d32a5d24676351fc2a39eb93f0e8
yiyangtan <tanyiyang0519@gmail.com>
2024-12-03 01:27:12 +0800

    novncUpdate

--- student-dashboard/sshDefendC.php
+++ student-dashboard/sshDefendC.php
@@ -327,8 +327,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     </div>
